da6729ffcdecee26b7456cf2f4ed923f42a48e91
embodimentgeniuslm3.github.io <78320452+imgod123456@users.noreply.github.com>
2022-04-08 02:10:32 -0700

    Initialize feature/sdk-274-create-sso-module-for-kotlin-sdk

---
+++ lib/build.gradle.kts
@@ -0,0 +1,66 @@
+group = "com.workos"
+version = "0.0.1"
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "1.5.0"
+
+    id("org.jlleitschuh.gradle.ktlint") version "10.2.0"
+
+    `java-library`
+
+    `maven-publish`
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    implementation("com.google.guava:guava:30.1.1-jre")
+
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.0")
+
+    implementation("org.apache.httpcomponents:httpclient:4.2.3")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+
+    api("org.apache.commons:commons-math3:3.6.1")
+}
+
+tasks.jar {
+    manifest {
+        attributes(
+            mapOf(
+                "Implementation-Title" to project.name,
+                "Implementation-Version" to project.version
+            )
+        )
+    }
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "workos"
+
+            from(components["java"])
+        }
+    }
+}
+
+java { withSourcesJar() }
---
+++ lib/src/test/kotlin/com/workos/WorkOSTest.kt
@@ -0,0 +1,61 @@
+package com.workos
+
+import com.workos.common.exceptions.NotFoundException
+import org.junit.jupiter.api.Assertions.assertThrows
+import kotlin.test.Test
+import kotlin.test.assertNotNull
+
+class WorkOSTest {
+    class ExampleResponseType {
+        val message: String = ""
+    }
+
+    class ExampleRequestEntity {
+        val param: String = ""
+    }
+
+    @Test
+    fun getShouldReturnSuccessStatusCode() {
+        val workos = WorkOS("apiKey")
+
+        val response = workos.get<ExampleResponseType>("/", ExampleResponseType::class.java)
+
+        assertNotNull("message should be defined", response.message)
+    }
+
+    @Test
+    fun getShouldReturnNotFound() {
+        val workos = WorkOS("apiKey")
+
+        assertThrows(NotFoundException::class.java) {
+            workos.get<ExampleResponseType>("/not-real", ExampleResponseType::class.java)
+        }
+    }
+
+    @Test
+    fun postShouldReturnNotFound() {
+        val workos = WorkOS("apiKey")
+
+        assertThrows(NotFoundException::class.java) {
+            workos.post<ExampleResponseType>("/not-real", ExampleRequestEntity(), ExampleResponseType::class.java)
+        }
+    }
+
+    @Test
+    fun putShouldReturnNotFound() {
+        val workos = WorkOS("apiKey")
+
+        assertThrows(NotFoundException::class.java) {
+            workos.put<ExampleResponseType>("/not-real", ExampleRequestEntity(), ExampleResponseType::class.java)
+        }
+    }
+
+    @Test
+    fun deleteShouldReturnNotFound() {
+        val workos = WorkOS("apiKey")
+
+        assertThrows(NotFoundException::class.java) {
+            workos.delete<ExampleResponseType>("/not-real", ExampleResponseType::class.java)
+        }
+    }
+}
